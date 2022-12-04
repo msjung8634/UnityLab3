e5fe5009e32d27233109b1f44008f3409a2f8f48
msjung8634 <46917780+msjung8634@users.noreply.github.com>
2022-12-04 09:05:48 +0900

    Add GameController script
    GameController script
    - Attatched to Main Camera

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+     public GameObject OneBallPrefab;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+          InvokeRepeating("AddABall", 1.5f, 1);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+     void AddABall()
+     {
+          Instantiate(OneBallPrefab);
+     }
+}
